f86189fe7ef324bc055b737111ed3e4b85e15890
EKotikova <kotik04@gmail.com>
2021-12-05 21:20:17 +0300

    version 0.1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,9 +1,23 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.edge.EdgeDriver;
+
 public class Main {
     public  static  void main(String[] args){
         System.setProperty("webdriver.edge.driver", "C:\\edgedriver\\msedgedriver.exe");
         WebDriver driver = new EdgeDriver();
         driver.get("http://addressbook");
+        driver.findElement(By.name("user")).click();
+        driver.findElement(By.name("user")).clear();
+        driver.findElement(By.name("user")).sendKeys("admin");
+        driver.findElement(By.name("pass")).click();
+        driver.findElement(By.name("pass")).clear();
+        driver.findElement(By.name("pass")).sendKeys("secret");
+        driver.findElement(By.xpath("//input[@value='Войти']")).click();
+
+       
+
     }
+
+
 }
